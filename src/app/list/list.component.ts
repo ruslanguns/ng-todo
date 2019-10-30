662a7cb0810512ed13816c234865bae45aa1ec7b
kudoark <kudoark@gmail.com>
2019-10-30 13:59:35 +0530

    added edit feature

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { listItemModel } from "../list-item-model";
-import { ToastrService } from "ngx-toastr";
-import { trigger, transition, useAnimation } from "@angular/animations";
-import { bounce } from "ng-animate";
+import {  Component,  OnInit} from "@angular/core";
+import {  listItemModel} from "../list-item-model";
+import {  ToastrService} from "ngx-toastr";
+import {  trigger,  transition,  useAnimation} from "@angular/animations";
+import {  bounce} from "ng-animate";
 
 @Component({
   selector: "app-list",
@@ -14,6 +14,7 @@ export class ListComponent implements OnInit {
   bounce: any;
   item: string = "";
   listItems: listItemModel[] = [];
+  buttonname: string = "Add Item";
 
   constructor(private toastr: ToastrService) {}
 
@@ -27,11 +28,19 @@ export class ListComponent implements OnInit {
     } else {
       let randomId = Math.floor(100000 + Math.random() * 900000);
       this.listItems.push(new listItemModel(this.item, false, randomId));
+      if (this.buttonname == "Edit Item") {
+        this.toastr.success("Item: " + this.item, "Item Edited!", {
+          timeOut: 1500
+        });
 
-      this.toastr.success("Item: " + this.item, "Item Added!", {
-        timeOut: 1500
-      });
+      } else {
+
+        this.toastr.success("Item: " + this.item, "Item Added!", {
+          timeOut: 1500
+        });
+      }
       this.item = "";
+      this.buttonname = "Add Item";
     }
   }
 
@@ -40,5 +49,12 @@ export class ListComponent implements OnInit {
       timeOut: 1500
     });
     this.listItems = this.listItems.filter(itemID => itemID.id != item.id);
+  }
+
+  editItem(item: any) {
+    this.item = item.itemName;
+    this.buttonname = "Edit Item";
+    this.listItems = this.listItems.filter(itemID => itemID.id != item.id);
+
   }
 }
